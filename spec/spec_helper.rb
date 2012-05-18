0dca11eb381bb961833390ea45b7bbe8a25b20e6
Kazuya Takeshima <mail@mitukiii.jp>
2012-05-19 02:39:26 +0900

    Add simplecov gem to development dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+# coding: utf-8
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'spec'
+end
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
